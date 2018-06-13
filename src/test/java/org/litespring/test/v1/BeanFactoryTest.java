76a9d4c3ef4162ed5266371126c41a69440a2370
qin <qin@qindeMacBook-Pro.local>
2018-06-13 23:20:12 +0800

    Commit for building the embryonic form framework

---
+++ src/test/java/org/litespring/test/v1/BeanFactoryTest.java
@@ -0,0 +1,12 @@
+package org.litespring.test.v1;
+
+import org.junit.Test;
+
+public class BeanFactoryTest {
+
+    @Test
+    public void testGetBean(){
+        System.out.println("start");
+    }
+
+}
